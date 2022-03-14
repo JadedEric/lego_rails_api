e84fe37c856468c77abff7990446c1eb0aab2d96
Eric Truter <eric.truter@live.com>
2022-03-14 23:36:20 +0200

    fix: remove null: true from migration

--- db/migrate/20220314212427_create_scrape.rb
+++ db/migrate/20220314212427_create_scrape.rb
@@ -1,11 +1,11 @@
 class CreateScrape < ActiveRecord::Migration[7.0]
   def change
     create_table :scrapes do |t|      
-      t.string :url
-      t.datetime :date
-      t.integer :status
-      t.integer :set_id, null: true
-      t.string :keyword, null: true
+      t.string :url, null: false
+      t.datetime :date, null: false
+      t.integer :status, null: false
+      t.integer :set_id
+      t.string :keyword
       t.datetime :created_date
       t.integer :created_by
       t.datetime :modified_date
